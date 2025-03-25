e6309577ebe4bfb1e9f846613c5970ffe84b0394
Kakarot <manjeetkumar1572000@gmail.com>
2025-03-25 17:45:44 +0530

    update files

--- node/Ecommerce/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signUp, login, logout } from '../controllers/userController.js';
-const userRouter = express.Router();
-
-userRouter.post('/signup', signUp);
-userRouter.post('/login', login);
-userRouter.get('/logout', logout);
-
-export default userRouter;
--- node/RecipeAPI/src/models/Comment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const CommentSchema = new mongoose.Schema({
-    text: String,
-    recipeId: { type: mongoose.Schema.Types.ObjectId, ref: "Recipe" },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-}, { timestamps: true });
-
-export default mongoose.model("Comment", CommentSchema);
